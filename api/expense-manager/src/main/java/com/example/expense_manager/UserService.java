1ad5f71a94fa0bbfa3071e827aa1eaa183b4b9ae
azizam <azizam90@yahoo.com.my>
2024-07-28 01:21:58 +0800

    Test simple login,register and database

---
+++ api/expense-manager/src/main/java/com/example/expense_manager/UserService.java
@@ -0,0 +1,19 @@
+package com.example.expense_manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
